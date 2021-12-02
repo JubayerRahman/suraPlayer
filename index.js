7b1115f8aadd6c8155db369884e50d180bac1da5
JubayerRahman <68602999+JubayerRahman@users.noreply.github.com>
2021-12-02 23:13:34 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+var lan =document.querySelectorAll(".surah").length;
+
+for(var i=0; i<lan; i++){
+    document.querySelectorAll(".surah")[i].addEventListener("click", function(){
+        var text = this.innerHTML;
+        console.log(text);
+        animation(text);
+
+        
+        
+
+        switch(text){
+            case "AlIkhlas":
+            var audio = new Audio("surah/112-al-ikhlas.mp3");
+            audio.play();
+            break;
+
+            case "AlFalaq":
+            var audio1 = new Audio("surah/113-al-falaq.mp3");
+            audio1.play();
+            break;
+
+            case "AlNas":
+            var audio2 = new Audio("surah/114-an-nas.mp3");
+            audio2.play();
+            break;
+
+        }
+    })
+
+    function animation(text){
+
+        var animeButton = document.querySelector("."+text);
+        animeButton.classList.add("anime");
+
+        setTimeout(function(){
+            animeButton.classList.remove("anime");
+        }, 1000);
+
+        }
+        
+        
+    }
