7b8a4c97f5adddfa522ea462cbf5143b99d545a9
JubayerRahman <68602999+JubayerRahman@users.noreply.github.com>
2021-12-02 23:19:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ for(var i=0; i<lan; i++){
 
         switch(text){
             case "AlIkhlas":
-            var audio = new Audio("surah/112-al-ikhlas.mp3");
+            var audio = new Audio("112-al-ikhlas.mp3");
             audio.play();
             break;
 
             case "AlFalaq":
-            var audio1 = new Audio("surah/113-al-falaq.mp3");
+            var audio1 = new Audio("113-al-falaq.mp3");
             audio1.play();
             break;
 
             case "AlNas":
-            var audio2 = new Audio("surah/114-an-nas.mp3");
+            var audio2 = new Audio("114-an-nas.mp3");
             audio2.play();
             break;
 
